1ca06c4b85cd6279f16d511168e8b84858de500d
Kateryna Strough <katerynastrough@hotmail.com>
2020-02-22 14:51:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { render } from 'react-dom';
-import App from './containers/App';
+import { NavLink } from 'react-router-dom';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+const NavBar = () => {
+  return (
+    <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
+      <NavLink 
+        style={{ marginRight: '10px' }} 
+        to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink 
+        style={{ marginRight: '10px' }} 
+        to="/movies"
+      >
+        Movies
+      </NavLink>
+    </div>
+  );
+}
+
+export default NavBar;
